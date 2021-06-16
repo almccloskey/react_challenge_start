3b3c1d921f80fd9d897c793126829ae7ab243e04
[almccloskey@me.com] <60784483+almccloskey@users.noreply.github.com>
2021-06-15 19:08:10 -0500

    defining state variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -10,6 +10,9 @@ function App() {
   contacts and appointments 
   */
 
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+ 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
