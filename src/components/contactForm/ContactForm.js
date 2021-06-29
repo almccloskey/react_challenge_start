2413d56a4eb5bddbfde9fefa535e6537a7ebbb6b
[almccloskey@me.com] <60784483+almccloskey@users.noreply.github.com>
2021-06-29 17:00:15 -0500

    contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,23 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input value={name}
+        type='text' 
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input value={phone}
+        type='tel'
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+      />
+      <input value={email}
+        type='email'
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type='submit'>
+        Submit
+      </button>
+    </form>
   );
 };
